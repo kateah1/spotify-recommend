ac3ffbb0e37f32991be9a7d034e4acb6830d40c0
kateah1 <kate.hurd64@gmail.com>
2016-09-26 14:17:09 -0400

    217

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+// unirest is a library to simplify making http rest requests
 var unirest = require('unirest');
+// express is a fast and minimalist web framework 
 var express = require('express');
+// eventEmmiter.on() is used to register listeners
+// eventEmmiter.emit() is used to trigger events
 var events = require('events');
 
 var getFromApi = function (endpoint, args) {
+  // create event emmitter to communicate if request was successful or failed
   var emitter = new events.EventEmitter();
+  // make unirest http get request using endpoint
   unirest.get('https://api.spotify.com/v1/' + endpoint)
+    // append querystring to URL
     .qs(args)
+    // call 'end' to send request
     .end(function (response) {
       if (response.ok) {
+        // trigger end event and attach the response
         emitter.emit('end', response.body);
-      }else {
+      } else {
+        // trigger error event by emmitter, attach error code return by unirest
         emitter.emit('error', response.code);
       }
     });
@@ -17,24 +27,37 @@ var getFromApi = function (endpoint, args) {
 };
 
 var app = express();
+// middle ware (software used as a bridge btwn database and app)
+// used to serve static files (image, css, js) from specified directory
 app.use(express.static('public'));
 
 app.get('/search/:name', function (req, res) {
   var searchReq = getFromApi('search', {
+    // arguments to add to endpoint
     q: req.params.name,
     limit: 1,
     type: 'artist'
   });
 
-  // listeners to event emitter returned from getFromApi
+  // register listener to event emitter returned from getFromApi
   searchReq.on('end', function (item) {
     var artist = item.artists.items[0];
-    unirest.get(artist)
+    unirest.get('https://api.spotify.com/v1/artists/' + artist.id + '/related-artists')
+      .end(function (response) {
+        console.log(response.body.artists);
+        if (response.ok) {
+          artist.related = response.body.artists;
+          res.json(artist);
+        } else {
+          res.sendStatus(404);
+        }
+      });
+    unirest.get('https://api.spotify.com/v1/artists/' + artist.related.id + '/top-tracks?country=US')
       .end(function (response) {
         if (response.ok) {
-          artist.related === item.artists;
+          artist.related.tracks = response.body.item.tracks;
           res.json(artist);
-        }  else {
+        } else {
           res.sendStatus(404);
         }
       });
